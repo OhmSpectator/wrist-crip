1d885c6b512423ae3befef2ae754a64215a7a792
Nikolay Martyanov <ohmspectator@gmail.com>
2017-01-12 14:08:00 +0000

    Add mock for show_mode standalone

---
+++ examples/standalone/show_mode.c
@@ -0,0 +1,12 @@
+//
+// Created by Nikolay Martyanov on 12/01/2017.
+//
+
+#include <common.h>
+
+int show_mode(int argc, char* const argv[])
+{
+    //Read processor mode from CPSR
+    printf("%s", "Here the processor mode will be shown\n");
+    return 0;
+}
