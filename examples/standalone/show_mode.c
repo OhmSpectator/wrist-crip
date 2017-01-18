f334ec947ac3f9586c256f6f991fd1b2bd0ceca6
Nikolay Martyanov <ohmspectator@gmail.com>
2017-01-18 23:29:26 +0300

    Code clean-up

--- examples/standalone/show_mode.c
+++ examples/standalone/show_mode.c
@@ -5,57 +5,28 @@
 #include <common.h>
 #include <inttypes.h>
 
-#define CONFIG_SWITCH_VIA_SMC   0
-
-#define SMC_VECTOR_OFFSET  0x8
-#define START_ADDRESS      0x81000000
-#define MON_VECTOR_ADDRESS 0x82000000
-#define MOD_MASK           0b11111
-#define MON_MODE           0b10110
+#define MOD_MASK 0b11111
+#define MON_MODE 0b10110
+#define HYP_MODE 0b11010
 
 
 extern uint32_t* _monitor_vectors;
 
 
-int show_mode(int argc, char* const argv[]) {
-    printf("Press any button to read MVBAR\n");
+int show_mode(int argc, char* const argv[])
+{
+    printf("Press any button to read ID_PFR1\n");
     (void) getc();
-    uint32_t mvbar_val;
+    uint32_t id_pfr1_val;
     asm volatile(
-        "mrc p15, 0, r0, c12, c0, 1 \n"
+        "mrc p15, 0, r0, c0, c1, 1 \n"
         "str r0, %0"
-        : "=m" (mvbar_val)
+        : "=m" (id_pfr1_val)
         :
         : "r0"
     );
+    printf("ID_PFR1: 0x%08" PRIx32 "\n", id_pfr1_val);
     printf("Done!\n");
-    printf("MVBAR: 0x%" PRIx32 "\n", mvbar_val);
-
-#if CONFIG_SWITCH_VIA_SMC
-    printf("Press any button to fix MVBAR\n");
-    (void) getc();
-    asm volatile(
-        "ldr r0, %0 \n"
-        "mcr p15, 0, r0, c12, c0, 1 \n"
-        "isb"
-        :
-        : "m" (_monitor_vectors)
-        : "r0"
-    );
-    printf("Done!\n");
-
-    printf("Press any button to read MVBAR\n");
-    (void) getc();
-    asm volatile(
-        "mrc p15, 0, r0, c12, c0, 1 \n"
-        "str r0, %0"
-        : "=m" (mvbar_val)
-        :
-        : "r0"
-    );
-    printf("MVBAR: 0x%" PRIx32 "\n", mvbar_val);
-    printf("Done!\n");
-#endif
 
     printf("Press any button to read SCTLR\n");
     (void) getc();
@@ -80,21 +51,9 @@ int show_mode(int argc, char* const argv[]) {
         :
         : "r0"
     );
-    printf("CPSR: 0x%"  PRIx32 "\n", cpsr_val);
+    printf("CPSR: 0x%08" PRIx32 "\n", cpsr_val);
     printf("Done!\n");
 
-    printf("Press any button to read ID_PFR1\n");
-    (void) getc();
-    uint32_t id_pfr1_val;
-    asm volatile(
-        "mrc p15, 0, r0, c0, c1, 1 \n"
-        "str r0, %0                  "
-        : "=m" (id_pfr1_val)
-        :
-        : "r0"
-    );
-    printf("ID_PFR1: 0x%" PRIx32 "\n", id_pfr1_val);
-    printf("Done!\n");
 
     printf("Press any button to read SCR\n");
     (void) getc();
@@ -109,19 +68,9 @@ int show_mode(int argc, char* const argv[]) {
     printf("SCR: 0x%08" PRIx32 "\n", scr_val);
     printf("Done!\n");
 
-#if CONFIG_SWITCH_VIA_SMC
-    printf("Press any button to switch to MON mode (via smc)\n");
-    asm volatile(
-        "smc #0"
-        :
-        :
-        :
-    );
-    asm("_smc_ret:");
-#else
     printf("Press any button to switch to MON mode (via CPSR fix)\n");
     (void)getc();
-    uint32_t mon_mode = MON_MODE;
+    uint32_t proc_mode = MON_MODE;
     uint32_t mode_mask = ~MODE_MASK;
     asm volatile(
         "mrs r0, cpsr \n"
@@ -135,11 +84,11 @@ int show_mode(int argc, char* const argv[]) {
         "mov sp, r1 \n"
         "mov lr, r2"
         :
-        : "m" (mon_mode), "m" (mode_mask)
+        : "m" (proc_mode), "m" (mode_mask)
         : "r0", "r1", "r2", "cc", "memory"
     );
-#endif
     printf("Done!\n");
+
     printf("Press any button to read CPSR\n");
     (void) getc();
     asm volatile(
@@ -149,7 +98,7 @@ int show_mode(int argc, char* const argv[]) {
         :
         : "r0"
     );
-    printf("CPSR: 0x%"  PRIx32 "\n", cpsr_val);
+    printf("CPSR: 0x%08" PRIx32 "\n", cpsr_val);
     printf("Done!\n");
 
     printf("Press any button to set SCR.NS to 1\n");
@@ -169,11 +118,11 @@ int show_mode(int argc, char* const argv[]) {
     printf("Press any button to read SCR\n");
     (void) getc();
     asm volatile(
-    "mrc p15, 0, r0, c1, c1, 0 \n"
-            "str r0, %0                  "
-    : "=m" (scr_val)
-    :
-    : "r0"
+        "mrc p15, 0, r0, c1, c1, 0 \n"
+        "str r0, %0                  "
+        : "=m" (scr_val)
+        :
+        : "r0"
     );
     printf("SCR: 0x%08" PRIx32 "\n", scr_val);
     printf("Done!\n");
@@ -188,8 +137,12 @@ int show_mode(int argc, char* const argv[]) {
         :
         : "r0"
     );
-    printf("HVBAR: 0x%" PRIx32 "\n", hvbar_val);
+    printf("HVBAR: 0x%08" PRIx32 "\n", hvbar_val);
     printf("Done!\n");
+
+    printf("Press \'s\' to exit, any other button to continue\n");
+    if(getc() == 's')
+        return 0;
 
     printf("Press any button to enter HYP mode (via hvc)\n");
     (void) getc();
@@ -211,25 +164,9 @@ int show_mode(int argc, char* const argv[]) {
         : "r0"
     );
     printf("Done!\n");
-    printf("CPSR: 0x%"  PRIx32 "\n", cpsr_val);
+    printf("CPSR: 0x%08" PRIx32 "\n", cpsr_val);
     (void) getc();
 
     return 0;
 }
-
-#if CONFIG_SWITCH_VIA_SMC
-asm (
-".global _monitor_vectors \n"
-        ".align	5                 \n"
-        "_monitor_vectors:        \n"
-        ".word 0                  \n"
-        ".word 0                  \n"
-        "adr pc, _smc_ret         \n"
-        ".word 0                  \n"
-        ".word 0                  \n"
-        ".word 0                  \n"
-        ".word 0                  \n"
-        ".word 0                  \n"
-);
-#endif
 
